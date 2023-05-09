da8ef9432dd2f8418d4e26222f93bbf39c92e11d
L.Kit <64409723+link2868@users.noreply.github.com>
2023-05-09 20:31:29 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,8 @@
-// import tabs from "./modules/tabs.js";
+import tabs from "./modules/tabs.js";
 
-// window.addEventListener('DOMContentLoaded', () => {
-//     'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
 
-//     tabs();
+    tabs();
         
-// });
-
-function tabs() {
-  const tabs = document.querySelectorAll(".tabs_point"),
-  tabsContent = document.querySelectorAll(".tabcontent"),
-  tabsParent = document.querySelector(".tabs_parent");
-
-function hideContent() {
-  tabsContent.forEach((element) => {
-    element.classList.add("hide");
-    element.classList.remove("show", "fade");
-  });
-  tabs.forEach((el) => {
-    el.classList.remove("active");
-  });
-}
-
-function showContent(i = 0) {
-  tabsContent[i].classList.add("show", "fade");
-  tabsContent[i].classList.remove("hide");
-  tabs[i].classList.add("active");
-}
-
-hideContent();
-showContent();
-
-tabsParent.addEventListener("click", (item) => {
-  if (item.target && item.target.classList.contains("tabs_point"))  {
-    tabs.forEach((el, i) => {
-      if (item.target == el) {
-        hideContent();
-        showContent(i);
-      }
-    });
-  }
 });
-}
-tabs();
