d7fda16ef13f8b01506b2b6f268001fe719229aa
L.Kit <link2868@ukr.net>
2023-05-17 10:14:41 +0300

    inputs вiдправка форми ajax

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
 import tabs from "./modules/tabs.js";
 import inputs from "./modules/inputs.js";
 
-window.addEventListener('DOMContentLoaded', () => {
-    'use strict';
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
 
-    tabs(".tabs_point", ".tabcontent", ".tabs_parent");
-    inputs();
-        
+  tabs(".tabs_point", ".tabcontent", ".tabs_parent");
+  inputs();
 });
--- js/modules/inputs.js
+++ js/modules/inputs.js
@@ -1,29 +1,92 @@
+const ajax = (data) => {
+  // let body = JSON.stringify(data);
+  let body = data;
+
+  let xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://svitlo.oe.if.ua/GavGroupByAccountNumber", true);
+  xhr.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded; charset=UTF-8"
+  );
+  // xhr.open("POST", "https://reqres.in/api/users", true);
+
+  xhr.send(body);
+  console.log(body);
+
+  xhr.onload = function () {
+    console.log(xhr.readyState);
+    if (xhr.status === 200) {
+      console.log("Post successfully created!");
+      console.log(xhr.response);
+      document.querySelector(".inform").innerHTML = xhr.response;
+    }
+  };
+};
+
 function inputs() {
-  function formaPoNumber(element) {
+  function formaPoAccount(element) {
     document.querySelector(element).addEventListener("submit", (event) => {
       event.preventDefault();
       const form = document.querySelector(element),
         value = form.elements.input.value;
 
       console.log(value);
-      form.reset();
+
+      const body =
+        "accountNumber=" +
+        value +
+        "&" +
+        "userSearchChoice=" +
+        "pob" +
+        "&" +
+        "address=" +
+        " ";
+      // const body = {
+      //   accountNumber: value,
+      //   userSearchChoice: "pob",
+      //   address: "",
+      // };
+
+      ajax(body);
+    });
+  }
+
+  function formaPoNumber(element) {
+    document.querySelector(element).addEventListener("submit", (event) => {
+      event.preventDefault();
+      const form = document.querySelector(element),
+        value = form.elements.input.value;
+
+      // console.log(value);
+
+      const body = {
+        accountNumber: value,
+        userSearchChoice: "pob",
+        address: "",
+      };
+
+      ajax(body);
     });
   }
 
   function formPoaddress(element) {
     document.querySelector(element).addEventListener("submit", (event) => {
       event.preventDefault();
-      const form = document.querySelector(element),
-        value = new Object();
-      value.city = form.elements.city.value;
-      value.street = form.elements.street.value;
-      value.house = form.elements.house.value;
+      const form = document.querySelector(element);
+      const address = `${form.elements.city.value},${form.elements.street.value},${form.elements.house.value}`;
 
-      console.log(value);
+      const body = {
+        accountNumber: "",
+        userSearchChoice: "pob",
+        address,
+      };
+
+      // console.log(value);
+      ajax(body);
     });
   }
 
-  formaPoNumber("#account");
+  formaPoAccount("#account");
   formaPoNumber("#number");
   formPoaddress("#address");
 }
