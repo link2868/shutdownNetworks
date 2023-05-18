b0764948bf975d34674f52a8a513b160c72fc5e3
L.Kit <link2868@ukr.net>
2023-05-18 16:01:53 +0300

    inputs вiдправка форми get - fetch

--- js/modules/inputs.js
+++ js/modules/inputs.js
@@ -1,26 +1,24 @@
-const ajax = (data) => {
-  // let body = JSON.stringify(data);
-  let body = data;
-
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "https://svitlo.oe.if.ua/GavGroupByAccountNumber", true);
-  xhr.setRequestHeader(
-    "Content-Type",
-    "application/x-www-form-urlencoded; charset=UTF-8"
-  );
-  // xhr.open("POST", "https://reqres.in/api/users", true);
-
-  xhr.send(body);
-  console.log(body);
-
-  xhr.onload = function () {
-    console.log(xhr.readyState);
-    if (xhr.status === 200) {
-      console.log("Post successfully created!");
-      console.log(xhr.response);
-      document.querySelector(".inform").innerHTML = xhr.response;
+const DefineQueue = async () => {
+  let response = await fetch(
+    "https://svitlo.oe.if.ua/GAVTurnOff/RemFilterP?remList=bog_p",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      // body: data,
     }
-  };
+  );
+  if (response.ok) {
+    console.log("Post successfully created!");
+
+    let result = await response.text();
+    document.querySelector(".inform").innerHTML = result;
+  } else {
+    console.log("response " + response.status + "   " + response.statusText);
+
+    document.querySelector(".inform").innerHTML = "Спробуйте пізніше!";
+  }
 };
 
 function inputs() {
@@ -40,14 +38,9 @@ function inputs() {
         "pob" +
         "&" +
         "address=" +
-        " ";
-      // const body = {
-      //   accountNumber: value,
-      //   userSearchChoice: "pob",
-      //   address: "",
-      // };
+        "";
 
-      ajax(body);
+      DefineQueue(body);
     });
   }
 
@@ -57,15 +50,17 @@ function inputs() {
       const form = document.querySelector(element),
         value = form.elements.input.value;
 
-      // console.log(value);
+      const body =
+        "accountNumber=" +
+        value +
+        "&" +
+        "userSearchChoice=" +
+        "jur" +
+        "&" +
+        "address=" +
+        "";
 
-      const body = {
-        accountNumber: value,
-        userSearchChoice: "pob",
-        address: "",
-      };
-
-      ajax(body);
+      DefineQueue(body);
     });
   }
 
@@ -75,14 +70,17 @@ function inputs() {
       const form = document.querySelector(element);
       const address = `${form.elements.city.value},${form.elements.street.value},${form.elements.house.value}`;
 
-      const body = {
-        accountNumber: "",
-        userSearchChoice: "pob",
-        address,
-      };
+      const body =
+        "accountNumber=" +
+        "" +
+        "&" +
+        "userSearchChoice=" +
+        "pob" +
+        "&" +
+        "address=" +
+        address;
 
-      // console.log(value);
-      ajax(body);
+      DefineQueue(body);
     });
   }
 
