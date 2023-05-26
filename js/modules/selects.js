57c17a0ed6773ae27c6e7ba8e71d8036ac97341c
L.Kit <link2868@ukr.net>
2023-05-26 17:55:43 +0300

    select фiлiя пат catch

--- js/modules/selects.js
+++ js/modules/selects.js
@@ -1,22 +1,51 @@
 import preloader from "./preloader.js";
 
-const defineDisabledConsumers = async (url) => {
-  const statusMessage = preloader(),
-    response = await fetch(url),
-    informPanel = document.querySelector(".inform");
+const disconnectionByBranch = (url) => {
+  const informPanel = document.querySelector(".inform"),
+    statusMessage = preloader();
 
-  if (!response.ok) {
-    informPanel.innerHTML = `Помилка за адресою  ${url}, статус помилки  ${response.text()}`;
-    informPanel.scrollIntoView();
-    statusMessage.remove();
-  }
-  console.log("Post successfully created!");
-  let result = await response.text();
-  informPanel.innerHTML = `${result}`;
-  informPanel.scrollIntoView();
+  fetch(url)
+    .then((response) => {
+      let result = response.text();
+      return result;
+    })
+    .then((result) => {
+      const parser = new DOMParser(),
+        xmlDoc = parser.parseFromString(result, "text/xml");
 
-  statusMessage.remove();
+      informPanel.innerHTML = `${xmlDoc.documentElement.textContent}`;
+      informPanel.scrollIntoView();
+    })
+    .catch((error) => {
+      // informPanel.innerHTML = "ERROR";
+      informPanel.scrollIntoView();
+      console.error(error.response);
+    })
+    .finally(() => {
+      statusMessage.remove();
+      // setTimeout(() => {
+      //   informPanel.innerHTML = "";
+      // }, 5000);
+    });
 };
+
+// const disconnectionByBranch = async (url) => {
+//   const statusMessage = preloader(),
+//     response = await fetch(url),
+//     informPanel = document.querySelector(".inform");
+
+//   if (!response.ok) {
+//     informPanel.innerHTML = `Помилка за адресою  ${url}, статус помилки  ${response.text()}`;
+//     informPanel.scrollIntoView();
+//     statusMessage.remove();
+//   }
+//   console.log("Post successfully created!");
+//   let result = await response.text();
+//   informPanel.innerHTML = `${result}`;
+//   informPanel.scrollIntoView();
+
+//   statusMessage.remove();
+// };
 
 function selects() {
   const selectBranch = document.querySelector('[name="branch_pat"]');
@@ -25,10 +54,10 @@ function selects() {
     const getValue = selectBranch.value;
 
     if (getValue !== "select_rem" && getValue !== "select_null") {
-      const url = `https://svitlo.oe.if.ua/GAVTurnOff/RemFilterP?remList=${getValue}`;
+      const url = `https://svitlo.oe.if.ua/GAVTurnOff/RemFilter?remList=${getValue}`;
       console.log(getValue);
       console.log(url);
-      defineDisabledConsumers(url);
+      disconnectionByBranch(url);
     }
   });
 }
